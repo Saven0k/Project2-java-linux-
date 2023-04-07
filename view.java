bb98f1206b81b2d63f1f137f00c44679896299ce
Savenok <Romasav2017@gmail.com>
2023-04-07 20:08:55 +0300

    veiw

---
+++ view.java
@@ -0,0 +1,11 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.sampled.AudioFormat.Encoding;
+
+public class view {
+
+    public void view(ArrayList toys) {
+        System.out.println(toys);
+    }
+}
