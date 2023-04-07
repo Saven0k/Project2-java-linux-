a85abceba71b26d55361ad440952ee583a6173b5
Savenok <Romasav2017@gmail.com>
2023-04-07 20:09:05 +0300

    toy

---
+++ toy.java
@@ -0,0 +1,45 @@
+﻿import java.util.ArrayList;
+
+public class toy {
+
+    private int id; // id
+    private String name; // Имя
+    private int amount; // Колличество
+    private int dropChance; // Шанс выпадения
+
+    public toy(int id, int amount, String name, int dropChance) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+        this.dropChance = dropChance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getDropChance() {
+        return dropChance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setDropChance(int dropChance) {
+        this.dropChance = dropChance;
+    }
+
+}
