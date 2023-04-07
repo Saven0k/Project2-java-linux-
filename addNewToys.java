55584016e1ac13430de661af400b1e953fc499b0
Savenok <Romasav2017@gmail.com>
2023-04-07 20:10:07 +0300

    add new toys

---
+++ addNewToys.java
@@ -0,0 +1,14 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class addNewToys {
+
+    public ArrayList add(int id, int amount, String name, int dropChance, ArrayList toys) {
+        toy newToy = new toy(id, amount, name, dropChance);
+        toys.add(newToy);
+        System.out.println("Toy was add!\n");
+        System.out.println(toys);
+        return toys;
+    }
+
+}
