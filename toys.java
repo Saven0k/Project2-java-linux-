a19eb9a56e47f320239f50b4b65ce42103c6ed56
Savenok <Romasav2017@gmail.com>
2023-04-07 20:09:17 +0300

    toys

---
+++ toys.java
@@ -0,0 +1,17 @@
+﻿import java.util.ArrayList;
+import java.util.List;
+
+public class toys{
+    private ArrayList<toy> toyss;
+    public toys(){
+        toyss = new ArrayList<toy>();
+    }
+
+    public ArrayList getToys() {
+        return toyss;
+
+    }
+    public void addToys(toy toy){
+        toyss.add(toy);
+    }
+}
